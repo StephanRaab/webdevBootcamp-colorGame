6c5040c7b9979070a77ca94e659c2c1ecdfce87e
StephanRaab <sarcartist@gmail.com>
2016-09-12 19:59:36 -0400

    color game video pt4
    random color generated

--- colorgame.js
+++ colorgame.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
-	var colors = generateRandomColors(6);
 
+	var colors = generateRandomColors(6);
 	var squares = document.querySelectorAll(".square");
 	var pickedColor = pickColor();
 	var colorDisplay = document.getElementById("colorDisplay");
 	var messageDisplay = document.querySelector("#message");
+	var h1 = document.querySelector("h1");
 
 	colorDisplay.textContent = pickedColor;
 
@@ -20,6 +20,7 @@ $(document).ready(function(){
 			if (clickedColor === pickedColor) {
 				messageDisplay.textContent = "Correct!";
 				changeColors(clickedColor);
+				h1.style.background = clickedColor;
 			} else {
 				this.style.background = '#232323';
 				messageDisplay.textContent = "Try Again";
@@ -34,7 +35,7 @@ $(document).ready(function(){
 			squares[i].style.background = color;
 		}
 	}
-	
+
 	function pickColor() {
 		var random = Math.floor(Math.random() * colors.length);
 		return colors[random];
@@ -42,8 +43,23 @@ $(document).ready(function(){
 
 	function generateRandomColors(num) {
 		//make an array
+		var arr = [];
 		//add num random colors to arr
+		for (var i = 0; i < num; i++) {
+			//get random color from randomColor function and push into array
+			arr.push(randomColor());
+		}
 		//return arr
+		return arr;
 	}
 
-});
+	function randomColor() {
+		//pick red from 0-255
+		var red = Math.floor(Math.random() * 256);
+		//pick green from 0-255
+		var green = Math.floor(Math.random() * 256);
+		//pick blue from 0-255
+		var blue = Math.floor(Math.random() * 256);
+		//return combined string
+		return "rgb(" + red + ", " + green + ", " + blue + ")";
+	}
