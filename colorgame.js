cd7cbdb5aec0eeaa6b14e012a685ef5e6d32b5ec
StephanRaab <sarcartist@gmail.com>
2016-10-02 14:37:51 -0400

    Color game part 9
    - js refactoring
    - splitting init function into multiple smaller functions

--- colorgame.js
+++ colorgame.js
@@ -1,28 +1,58 @@
 var numSquares = 6;
-var colors = generateRandomColors(numSquares);
+var colors = [];
+var pickedColor;
 var squares = document.querySelectorAll(".square");
-var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#resetGame");
 var modeButtons = document.querySelectorAll(".mode");
 
-for (var i = 0; i < modeButtons.length; i++) {
-	modeButtons[i].addEventListener("click", function() {
-		modeButtons[0].classList.remove("selectedButton");
-		modeButtons[1].classList.remove("selectedButton");
-		this.classList.add("selected");
+init();
 
-		//ternary operator version --> this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
-		if (this.textContent === "Easy") {
-			numSquares = 3;
-		} else {
-			numSquares = 6;
-		}
+function init() {
+	setupModeButtons();
+	setupSquares();
+	reset();
+}
 
-		reset();
-	})
+function setupModeButtons() {
+	for (var i = 0; i < modeButtons.length; i++) {
+		modeButtons[i].addEventListener("click", function() {
+			modeButtons[0].classList.remove("selectedButton");
+			modeButtons[1].classList.remove("selectedButton");
+			this.classList.add("selected");
+
+			//ternary operator version --> this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
+			if (this.textContent === "Easy") {
+				numSquares = 3;
+			} else {
+				numSquares = 6;
+			}
+			reset();
+		})
+	}
+}
+
+function setupSquares() {
+	for (var i = 0; i < squares.length; i++) {
+		//add click listeners to squares
+		squares[i].addEventListener("click", function(){
+			//grab color of clicked square
+				var clickedColor = this.style.background;
+			//compare color to background
+			if (clickedColor === pickedColor) {
+				messageDisplay.textContent = "Correct!";
+				changeColors(clickedColor);
+				h1.style.background = clickedColor;
+				//set new colors button to read as play again
+				resetButton.textContent = "Play Again?"
+			} else {
+				this.style.background = '#232323';
+				messageDisplay.textContent = "Try Again";
+			}
+		})
+	}
 }
 
 function reset() {
@@ -47,33 +77,9 @@ function reset() {
 	h1.style.background = "steelblue";
 }
 
-colorDisplay.textContent = pickedColor;
-
 resetButton.addEventListener("click", function() {
 	reset();
 })
-
-for (var i = 0; i < squares.length; i++) {
-	//add initial colors to squares
-	squares[i].style.background = colors[i];
-
-	//add click listeners to squares
-	squares[i].addEventListener("click", function(){
-		//grab color of clicked square
-			var clickedColor = this.style.background;
-		//compare color to background
-		if (clickedColor === pickedColor) {
-			messageDisplay.textContent = "Correct!";
-			changeColors(clickedColor);
-			h1.style.background = clickedColor;
-			//set new colors button to read as play again
-			resetButton.textContent = "Play Again?"
-		} else {
-			this.style.background = '#232323';
-			messageDisplay.textContent = "Try Again";
-		}
-	})
-}
 
 function changeColors(color) {
 	//loop through all squares
